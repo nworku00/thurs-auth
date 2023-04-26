72e223b51e08713a5a9987469501fcb8e4eceb4b
Nati Worku <natiworku1971@gmail.com>
2023-04-26 14:46:31 -0400

    adds auth

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const session = require('express-session');
+const passport = require('passport');
+require('./config/passport')(passport);
+const authRouter = require('./routes/auth');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,7 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+    session({
+      secret: 'thurs-auth',
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
+
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
