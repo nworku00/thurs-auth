078b7a818e6b11fc5762b36965a7c10bf72adfa0
Nati Worku <natiworku1971@gmail.com>
2023-05-01 14:35:45 -0400

    possible way to get seeders to run only once

--- config/config.js
+++ config/config.js
@@ -19,6 +19,8 @@ module.exports = {
     "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
     "host": process.env.DATABASE_HOST,
-    "dialect": "postgres"
+      "dialect": "postgres",
+      "seederStorage": "sequelize",
+      'seederStorageTableName': 'sequelize_seeders'
   }
 }
